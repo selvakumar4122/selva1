8f7090778a0efe490232a60cabacba0b04419092
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-01-25 16:56:35 +0530

    Update VOWEL OR CONSONANT.C

--- VOWEL OR CONSONANT.C
+++ VOWEL OR CONSONANT.C
@@ -2,11 +2,11 @@
 void main()
 {
     char a;
-    printf("Enter the value for a:");
+    printf("Enter the character for a:");
     scanf("%c",&a);
     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
-        printf("the value is vowel");
+        printf("the character is vowel");
         else
-        printf("the value is consonant");
+        printf("the  character is consonant");
 }
 
