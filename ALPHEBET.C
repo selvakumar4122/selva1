e70fdb6ef2d0065f0299ef73dedb04aee8567c08
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-01-26 10:23:33 +0530

    Create ALPHEBET.C

---
+++ ALPHEBET.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    printf("Enter the character for a:");
+    scanf("%c",&a);
+    if('A'<=a<='z')
+        printf("the character ia Alphabet");
+        else
+        printf("the  character is not Alphabet");
+}
