48a1fae7217499cdb22039d36cad9906899e3dce
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-01-30 16:40:41 +0530

    Update grater among three number.c

--- grater among three number.c
+++ grater among three number.c
@@ -4,10 +4,10 @@ void main()
     int a,b,c;
     printf("Enter the values for a,b,c:");
     scanf("%d%d%d",&a,&b,&c); 
-   if(a>=b&&a>=c)
-        printf("The value of a is grater");
-    if(b>=a&&b>=c)
-        printf("the value b is gratter");
-    if(c>=a&&c>=b)
-         printf("the value c is gratter");
+   if(a>b&&a>c)
+        printf("The value %d is grater",a);
+   else if(b>c)
+        printf("the value %d is gratter",b);
+    else
+         printf("the value %d is gratter",c);
 }
