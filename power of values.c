b438f6e568d0e4db86d163e4a48f1c184ca87caf
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-01 16:19:33 +0530

    Create power of values.c

---
+++ power of values.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+  int a,n,c;
+  printf("enter value for a:");
+  scanf("%d",&a);
+   printf("enter value for power:");
+   scanf("%d",&n);
+   c=pow(a,n);
+  printf("the number of digits in integer:%d",c);
+}
+  
+
