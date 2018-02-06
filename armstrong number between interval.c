63426caeb107b2e5ccd4221cfcf200dc4dd1b6ba
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-06 17:00:37 +0530

    Create armstrong number between interval.c

---
+++ armstrong number between interval.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c,sum=0;
+    printf("enter valve for a,b:");
+    scanf("%d%d",&a,&b);
+    
+    while(a<b)
+{ for()
+    c=a%10;
+    sum=sum+(c*c*c);
+    a/=10;
+}
+if(b==sum)
+printf("%d",b);
+}
