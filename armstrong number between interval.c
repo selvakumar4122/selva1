6dd06bc7f13d4e2918a79daecbd0505774ae7a38
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-06 21:41:08 +0530

    Update armstrong number between interval.c

--- armstrong number between interval.c
+++ armstrong number between interval.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 void main()
 {
-    int a,b,c,sum=0;
-    printf("enter valve for a,b:");
-    scanf("%d%d",&a,&b);
-    
-    while(a<b)
-{ for()
-    c=a%10;
-    sum=sum+(c*c*c);
-    a/=10;
+    int a,b, i, temp1, temp2, remainder, n = 0, result = 0;
+    printf("Enter two numbers a,b: ");
+    scanf("%d %d",&a, &b);
+    printf("Armstrong numbers between %d an %d are: ",a,b);
+    for(i =a+1;i<b;++i)
+    {
+        temp2 = i;
+        temp1 = i;
+        while (temp1 != 0)
+        {
+            temp1 /= 10;
+            ++n;
+        }
+        while (temp2 != 0)
+        {
+            remainder = temp2 % 10;
+            result += pow(remainder, n);
+            temp2 /= 10;
+        }
+        if (result == i) 
+        {
+            printf("%d ", i);
+        }
+        n=0;
+        result =0;
+    }
 }
-if(b==sum)
-printf("%d",b);
-}
+
