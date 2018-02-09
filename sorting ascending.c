88df142da87af84e3a8976f542b492cff01e2357
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-09 15:47:50 +0530

    Create sorting ascending.c

---
+++ sorting ascending.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+  int a[1000],n,i,j,t;
+  printf("enter value for n:");
+  scanf("%d",&n);
+  printf("Enter the value for array : ");
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  printf("The sorted array is:");
+  for(i=0;i<n;i++)
+{
+    for(j=i+1;j<n;j++)
+    if(a[i]>a[j])
+    {
+     t=a[i];
+     a[i]=a[j];
+     a[j]=t;
+    }
+     printf("%d",a[i]);
+}
+}
