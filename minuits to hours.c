6d9c2a593ccde362cfe30d10189f8866dd9e88f8
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-13 16:31:24 +0530

    Create minuits to hours.c

---
+++ minuits to hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int mins,hour=0;
+    printf("Enter the minuits :");
+	scanf("%d",&mins);
+	if(mins<60)
+	{
+	    printf("%d %d",hour,mins);
+	}
+	else 
+	{
+	    hour=mins/60;
+	    mins=mins%60;
+	   	printf("%d Hours %d minuits",hour,mins);
+	}
+}
